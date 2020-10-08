276e0f1723bfb8d99f4ad7abbc212191fa951dc6
HoTuHi <hotuhi12@gmail.com>
2020-10-09 00:36:24 +0700

    huhu

--- js/test.js
+++ js/test.js
@@ -3,118 +3,122 @@ $(function(){
     var currentSong =0;
 //songs
     var songs =  [
-    {
-        artist: "unknown",
-        name:"7 Years__Lukas Graham__500" ,
-        url: "Musics/7 Years__Lukas Graham__500.m4a"},
-    {
-        artist:"Imagine Dragons",
-        name: "Bad Liar",
-        url: "Musics/Bad Liar__Imagine Dragons__320.mp3"},
-    {
-        artist: "unknown",
-        name:"Bánh Mì Không__Đạt G;   DuUyen__500" ,
-        url: "Musics/Bánh Mì Không__Đạt G;   DuUyen__500.m4a"},
-    {
-        artist:"Kina,  Adriana Proenza",
-        name: "Can We Kiss Forever?",
-        url: "Musics/Can We Kiss Forever __Kina; Adriana Proenza__320.mp3"},
-    {
-        artist: "unknown",
-        name:"Có Chàng Trai Viết Lên Cây__Phan Mạnh Quỳnh__500" ,
-        url: "Musics/Có Chàng Trai Viết Lên Cây__Phan Mạnh Quỳnh__500.m4a"},
-    {
-        artist: "unknown",
-        name:"Dancing" ,
-        url: "Musics/Dancing.m4a"},
-    {
-        artist:"Vương Thất Thất",
-        name: "Em Bằng Lòng Làm Một Người Bình Thường Ở Bên Cạnh Anh (我愿意平凡的陪在你身旁) DjYaha",
-        url: "Musics/Em Bằng Lòng Làm Một Người Bình Thường Ở Bên Cạnh Anh (我愿意平凡的陪在你身旁) DjYaha__Vương Thất Thất__320.mp3"},
-    {
-        artist:"Ngô Lan Hương",
-        name: "Hãy Trao Cho Anh (remix) - Ngô Lan Hương{Vinahouse}",
-        url: "Musics/Hãy Trao Cho Anh (remix) - Ngô Lan Hương{Vinahouse}__Ngô Lan Hương__320.mp3"},
-    {
-        artist: "unknown",
-        name:"Hết Thương Cạn Nhớ__Đức Phúc__500" ,
-        url: "Musics/Hết Thương Cạn Nhớ__Đức Phúc__500.m4a"},
-    {
-        artist:"Đạt G,   Du Uyên",
-        name: "Khó Vẽ Nụ Cười",
-        url: "Musics/Khó Vẽ Nụ Cười__Đạt G;    Du Uyên__320.mp3"},
-    {
-        artist: "unknown",
-        name:"Lily__Alan Walker; K-391; Emelie Hollow__500" ,
-        url: "Musics/Lily__Alan Walker; K-391; Emelie Hollow__500.m4a"},
-    {
-        artist: "unknown",
-        name:"Lost Control__Alan Walker; Sorana__500" ,
-        url: "Musics/Lost Control__Alan Walker; Sorana__500.m4a"},
-    {
-        artist: "unknown",
-        name:"Lấy Chồng Sớm Làm Gì (Acoustic Version)__Huy R;  Tuấn Cry__500" ,
-        url: "Musics/Lấy Chồng Sớm Làm Gì (Acoustic Version)__Huy R;  Tuấn Cry__500.m4a"},
-    {
-        artist: "unknown",
-        name:"Lối Nhỏ__Đen; Phương Anh Đào__500" ,
-        url: "Musics/Lối Nhỏ__Đen; Phương Anh Đào__500.m4a"},
-    {
-        artist:"Bán Dương",
-        name: "Một Khúc Tương Tư (一曲相思)",
-        url: "Musics/mktt.mp3"},
-    {
-        artist:"Dig DiDzay",
-        name: "Nếu Anh Đi Cover",
-        url: "Musics/NeuAnhDi.mp3"},
-    {
-        artist: "unknown",
-        name:"Người Lạ Ơi__Oi-Karik-Orange-Superbrothers__500" ,
-        url: "Musics/Người Lạ Ơi__Oi-Karik-Orange-Superbrothers__500.m4a"},
-    {
-        artist: "unknown",
-        name:"Nơi Em Muốn Tới__Xesi;Hoaprox__500" ,
-        url: "Musics/Nơi Em Muốn Tới__Xesi;Hoaprox__500.m4a"},
-    {
-        artist:"DIG DIDZAY",
-        name: "Nếu Em Đi (Cover)",
-        url: "Musics/Nếu Em Đi (Cover)__DIG DIDZAY__320.mp3"},
-    {
-        artist: "unknown",
-        name:"On My Way__Alan Walker;Sabrina Carpenter;Farruko__500" ,
-        url: "Musics/On My Way__Alan Walker;Sabrina Carpenter;Farruko__500.m4a"},
-    {
-        artist:"Ed Sheeran",
-        name: "Shape Of You",
-        url: "Musics/Shape Of You__Ed Sheeran__128.mp3"},
-    {
-        artist: "unknown",
-        name:"Ta Còn Yêu Nhau__Đức Phúc__500" ,
-        url: "Musics/Ta Còn Yêu Nhau__Đức Phúc__500.m4a"},
-    {
-        artist:"Olly Murs",
-        name: "That Girl",
-        url: "Musics/thatgirl.mp3"},
-    {
-        artist: "unknown",
-        name:"Tát Nhật Lãng Rực Rỡ (火红的萨日朗)__Yếu Bất Yếu Mãi Thái__500" ,
-        url: "Musics/Tát Nhật Lãng Rực Rỡ (火红的萨日朗)__Yếu Bất Yếu Mãi Thái__500.m4a"},
-    {
-        artist:"Ice Paper",
-        name: "Tâm Lặng Như Nước (心如止水)",
-        url: "Musics/Tâm Lặng Như Nước (心如止水)__Ice Paper__320.mp3"},
-    {
-        artist: "unknown",
-        name:"Vì Yêu Cứ Đâm Đầu__Min;   Đen;   JustaTee__500" ,
-        url: "Musics/Vì Yêu Cứ Đâm Đầu__Min;   Đen;   JustaTee__500.m4a"},
-    {
-        artist:"Avicii; Aloe Blacc",
-        name: "Wake Me Up",
-        url: "Musics/wakemeup.mp3"},
-    {
-        artist:"Avicii; Sandro Cavazza",
-        name: "Without You",
-        url: "Musics/withoutyou.mp3"}
+        { 
+            artist:"Naomi Scott", 
+            name: "Speechless (Full) (From Aladdin/Soundtrack Version)", 
+            url: "Musics/SpeechlessFullFromAladdinSoundtrackVersion-NaomiScott-6000942.mp3"},
+        { 
+            artist: "unknown",
+            name:"7 Years__Lukas Graham__500" , 
+            url: "Musics/7 Years__Lukas Graham__500.m4a"},
+        { 
+            artist:"Imagine Dragons", 
+            name: "Bad Liar", 
+            url: "Musics/Bad Liar__Imagine Dragons__320.mp3"},
+        { 
+            artist: "unknown",
+            name:"Bánh Mì Không__Đạt G;   DuUyen__500" , 
+            url: "Musics/Bánh Mì Không__Đạt G;   DuUyen__500.m4a"},
+        { 
+            artist:"Kina,  Adriana Proenza", 
+            name: "Can We Kiss Forever?", 
+            url: "Musics/Can We Kiss Forever __Kina; Adriana Proenza__320.mp3"},
+        { 
+            artist: "unknown",
+            name:"Có Chàng Trai Viết Lên Cây__Phan Mạnh Quỳnh__500" , 
+            url: "Musics/Có Chàng Trai Viết Lên Cây__Phan Mạnh Quỳnh__500.m4a"},
+        { 
+            artist: "unknown",
+            name:"Dancing" , 
+            url: "Musics/Dancing.m4a"},
+        { 
+            artist:"Vương Thất Thất", 
+            name: "Em Bằng Lòng Làm Một Người Bình Thường Ở Bên Cạnh Anh (我愿意平凡的陪在你身旁) DjYaha", 
+            url: "Musics/Em Bằng Lòng Làm Một Người Bình Thường Ở Bên Cạnh Anh (我愿意平凡的陪在你身旁) DjYaha__Vương Thất Thất__320.mp3"},
+        { 
+            artist:"Ngô Lan Hương", 
+            name: "Hãy Trao Cho Anh (remix) - Ngô Lan Hương{Vinahouse}", 
+            url: "Musics/Hãy Trao Cho Anh (remix) - Ngô Lan Hương{Vinahouse}__Ngô Lan Hương__320.mp3"},
+        { 
+            artist: "unknown",
+            name:"Hết Thương Cạn Nhớ__Đức Phúc__500" , 
+            url: "Musics/Hết Thương Cạn Nhớ__Đức Phúc__500.m4a"},
+        { 
+            artist:"Đạt G,   Du Uyên", 
+            name: "Khó Vẽ Nụ Cười", 
+            url: "Musics/Khó Vẽ Nụ Cười__Đạt G;    Du Uyên__320.mp3"},
+        { 
+            artist: "unknown",
+            name:"Lily__Alan Walker; K-391; Emelie Hollow__500" , 
+            url: "Musics/Lily__Alan Walker; K-391; Emelie Hollow__500.m4a"},
+        { 
+            artist: "unknown",
+            name:"Lost Control__Alan Walker; Sorana__500" , 
+            url: "Musics/Lost Control__Alan Walker; Sorana__500.m4a"},
+        { 
+            artist: "unknown",
+            name:"Lấy Chồng Sớm Làm Gì (Acoustic Version)__Huy R;  Tuấn Cry__500" , 
+            url: "Musics/Lấy Chồng Sớm Làm Gì (Acoustic Version)__Huy R;  Tuấn Cry__500.m4a"},
+        { 
+            artist: "unknown",
+            name:"Lối Nhỏ__Đen; Phương Anh Đào__500" , 
+            url: "Musics/Lối Nhỏ__Đen; Phương Anh Đào__500.m4a"},
+        { 
+            artist:"Bán Dương", 
+            name: "Một Khúc Tương Tư (一曲相思)", 
+            url: "Musics/mktt.mp3"},
+        { 
+            artist:"Dig DiDzay", 
+            name: "Nếu Anh Đi Cover", 
+            url: "Musics/NeuAnhDi.mp3"},
+        { 
+            artist: "unknown",
+            name:"Người Lạ Ơi__Oi-Karik-Orange-Superbrothers__500" , 
+            url: "Musics/Người Lạ Ơi__Oi-Karik-Orange-Superbrothers__500.m4a"},
+        { 
+            artist: "unknown",
+            name:"Nơi Em Muốn Tới__Xesi;Hoaprox__500" , 
+            url: "Musics/Nơi Em Muốn Tới__Xesi;Hoaprox__500.m4a"},
+        { 
+            artist:"DIG DIDZAY", 
+            name: "Nếu Em Đi (Cover)", 
+            url: "Musics/Nếu Em Đi (Cover)__DIG DIDZAY__320.mp3"},
+        { 
+            artist: "unknown",
+            name:"On My Way__Alan Walker;Sabrina Carpenter;Farruko__500" , 
+            url: "Musics/On My Way__Alan Walker;Sabrina Carpenter;Farruko__500.m4a"},
+        { 
+            artist:"Ed Sheeran", 
+            name: "Shape Of You", 
+            url: "Musics/Shape Of You__Ed Sheeran__128.mp3"},
+        { 
+            artist: "unknown",
+            name:"Ta Còn Yêu Nhau__Đức Phúc__500" , 
+            url: "Musics/Ta Còn Yêu Nhau__Đức Phúc__500.m4a"},
+        { 
+            artist:"Olly Murs", 
+            name: "That Girl", 
+            url: "Musics/thatgirl.mp3"},
+        { 
+            artist: "unknown",
+            name:"Tát Nhật Lãng Rực Rỡ (火红的萨日朗)__Yếu Bất Yếu Mãi Thái__500" , 
+            url: "Musics/Tát Nhật Lãng Rực Rỡ (火红的萨日朗)__Yếu Bất Yếu Mãi Thái__500.m4a"},
+        { 
+            artist:"Ice Paper", 
+            name: "Tâm Lặng Như Nước (心如止水)", 
+            url: "Musics/Tâm Lặng Như Nước (心如止水)__Ice Paper__320.mp3"},
+        { 
+            artist: "unknown",
+            name:"Vì Yêu Cứ Đâm Đầu__Min;   Đen;   JustaTee__500" , 
+            url: "Musics/Vì Yêu Cứ Đâm Đầu__Min;   Đen;   JustaTee__500.m4a"},
+        { 
+            artist:"Avicii; Aloe Blacc", 
+            name: "Wake Me Up", 
+            url: "Musics/wakemeup.mp3"},
+        { 
+            artist:"Avicii; Sandro Cavazza", 
+            name: "Without You", 
+            url: "Musics/withoutyou.mp3"}
     ]
     var rotaleImg = $("#circle-album");
     function showList(){
