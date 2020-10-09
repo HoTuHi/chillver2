004dc5926644ae200e3970316acdd0c5b7c1459a
HoTuHi <hotuhi12@gmail.com>
2020-10-09 13:14:57 +0700

    upperrrrr

--- js/test.js
+++ js/test.js
@@ -3,134 +3,133 @@ $(function(){
     var currentSong =0;
 //songs
     var songs =  [
-        { 
-            artist:"Naomi Scott", 
-            name: "Speechless (Full) (From Aladdin/Soundtrack Version)", 
+        {
+            artist:"Naomi Scott",
+            name: "Speechless (Full) (From Aladdin/Soundtrack Version)",
             url: "Musics/SpeechlessFullFromAladdinSoundtrackVersion-NaomiScott-6000942.mp3"},
-        { 
+        {
             artist: "unknown",
-            name:"7 Years__Lukas Graham__500" , 
+            name:"7 Years__Lukas Graham__500" ,
             url: "Musics/7 Years__Lukas Graham__500.m4a"},
-        { 
-            artist:"Imagine Dragons", 
-            name: "Bad Liar", 
+        {
+            artist:"Imagine Dragons",
+            name: "Bad Liar",
             url: "Musics/Bad Liar__Imagine Dragons__320.mp3"},
-        { 
+        {
             artist: "unknown",
-            name:"Bánh Mì Không__Đạt G;   DuUyen__500" , 
+            name:"Bánh Mì Không__Đạt G;   DuUyen__500" ,
             url: "Musics/Bánh Mì Không__Đạt G;   DuUyen__500.m4a"},
-        { 
-            artist:"Kina,  Adriana Proenza", 
-            name: "Can We Kiss Forever?", 
+        {
+            artist:"Kina,  Adriana Proenza",
+            name: "Can We Kiss Forever?",
             url: "Musics/Can We Kiss Forever __Kina; Adriana Proenza__320.mp3"},
-        { 
+        {
             artist: "unknown",
-            name:"Có Chàng Trai Viết Lên Cây__Phan Mạnh Quỳnh__500" , 
+            name:"Có Chàng Trai Viết Lên Cây__Phan Mạnh Quỳnh__500" ,
             url: "Musics/Có Chàng Trai Viết Lên Cây__Phan Mạnh Quỳnh__500.m4a"},
-        { 
+        {
             artist: "unknown",
-            name:"Dancing" , 
+            name:"Dancing" ,
             url: "Musics/Dancing.m4a"},
-        { 
-            artist:"Vương Thất Thất", 
-            name: "Em Bằng Lòng Làm Một Người Bình Thường Ở Bên Cạnh Anh (我愿意平凡的陪在你身旁) DjYaha", 
+        {
+            artist:"Vương Thất Thất",
+            name: "Em Bằng Lòng Làm Một Người Bình Thường Ở Bên Cạnh Anh (我愿意平凡的陪在你身旁) DjYaha",
             url: "Musics/Em Bằng Lòng Làm Một Người Bình Thường Ở Bên Cạnh Anh (我愿意平凡的陪在你身旁) DjYaha__Vương Thất Thất__320.mp3"},
-        { 
-            artist:"Ngô Lan Hương", 
-            name: "Hãy Trao Cho Anh (remix) - Ngô Lan Hương{Vinahouse}", 
+        {
+            artist:"Ngô Lan Hương",
+            name: "Hãy Trao Cho Anh (remix) - Ngô Lan Hương{Vinahouse}",
             url: "Musics/Hãy Trao Cho Anh (remix) - Ngô Lan Hương{Vinahouse}__Ngô Lan Hương__320.mp3"},
-        { 
+        {
             artist: "unknown",
-            name:"Hết Thương Cạn Nhớ__Đức Phúc__500" , 
+            name:"Hết Thương Cạn Nhớ__Đức Phúc__500" ,
             url: "Musics/Hết Thương Cạn Nhớ__Đức Phúc__500.m4a"},
-        { 
-            artist:"Đạt G,   Du Uyên", 
-            name: "Khó Vẽ Nụ Cười", 
+        {
+            artist:"Đạt G,   Du Uyên",
+            name: "Khó Vẽ Nụ Cười",
             url: "Musics/Khó Vẽ Nụ Cười__Đạt G;    Du Uyên__320.mp3"},
-        { 
+        {
             artist: "unknown",
-            name:"Lily__Alan Walker; K-391; Emelie Hollow__500" , 
+            name:"Lily__Alan Walker; K-391; Emelie Hollow__500" ,
             url: "Musics/Lily__Alan Walker; K-391; Emelie Hollow__500.m4a"},
-        { 
+        {
             artist: "unknown",
-            name:"Lost Control__Alan Walker; Sorana__500" , 
+            name:"Lost Control__Alan Walker; Sorana__500" ,
             url: "Musics/Lost Control__Alan Walker; Sorana__500.m4a"},
-        { 
+        {
             artist: "unknown",
-            name:"Lấy Chồng Sớm Làm Gì (Acoustic Version)__Huy R;  Tuấn Cry__500" , 
+            name:"Lấy Chồng Sớm Làm Gì (Acoustic Version)__Huy R;  Tuấn Cry__500" ,
             url: "Musics/Lấy Chồng Sớm Làm Gì (Acoustic Version)__Huy R;  Tuấn Cry__500.m4a"},
-        { 
+        {
             artist: "unknown",
-            name:"Lối Nhỏ__Đen; Phương Anh Đào__500" , 
+            name:"Lối Nhỏ__Đen; Phương Anh Đào__500" ,
             url: "Musics/Lối Nhỏ__Đen; Phương Anh Đào__500.m4a"},
-        { 
-            artist:"Bán Dương", 
-            name: "Một Khúc Tương Tư (一曲相思)", 
+        {
+            artist:"Bán Dương",
+            name: "Một Khúc Tương Tư (一曲相思)",
             url: "Musics/mktt.mp3"},
-        { 
-            artist:"Dig DiDzay", 
-            name: "Nếu Anh Đi Cover", 
+        {
+            artist:"Dig DiDzay",
+            name: "Nếu Anh Đi Cover",
             url: "Musics/NeuAnhDi.mp3"},
-        { 
+        {
             artist: "unknown",
-            name:"Người Lạ Ơi__Oi-Karik-Orange-Superbrothers__500" , 
+            name:"Người Lạ Ơi__Oi-Karik-Orange-Superbrothers__500" ,
             url: "Musics/Người Lạ Ơi__Oi-Karik-Orange-Superbrothers__500.m4a"},
-        { 
+        {
             artist: "unknown",
-            name:"Nơi Em Muốn Tới__Xesi;Hoaprox__500" , 
+            name:"Nơi Em Muốn Tới__Xesi;Hoaprox__500" ,
             url: "Musics/Nơi Em Muốn Tới__Xesi;Hoaprox__500.m4a"},
-        { 
-            artist:"DIG DIDZAY", 
-            name: "Nếu Em Đi (Cover)", 
+        {
+            artist:"DIG DIDZAY",
+            name: "Nếu Em Đi (Cover)",
             url: "Musics/Nếu Em Đi (Cover)__DIG DIDZAY__320.mp3"},
-        { 
+        {
             artist: "unknown",
-            name:"On My Way__Alan Walker;Sabrina Carpenter;Farruko__500" , 
+            name:"On My Way__Alan Walker;Sabrina Carpenter;Farruko__500" ,
             url: "Musics/On My Way__Alan Walker;Sabrina Carpenter;Farruko__500.m4a"},
-        { 
-            artist:"Ed Sheeran", 
-            name: "Shape Of You", 
+        {
+            artist:"Ed Sheeran",
+            name: "Shape Of You",
             url: "Musics/Shape Of You__Ed Sheeran__128.mp3"},
-        { 
+        {
             artist: "unknown",
-            name:"Ta Còn Yêu Nhau__Đức Phúc__500" , 
+            name:"Ta Còn Yêu Nhau__Đức Phúc__500" ,
             url: "Musics/Ta Còn Yêu Nhau__Đức Phúc__500.m4a"},
-        { 
-            artist:"Olly Murs", 
-            name: "That Girl", 
+        {
+            artist:"Olly Murs",
+            name: "That Girl",
             url: "Musics/thatgirl.mp3"},
-        { 
+        {
             artist: "unknown",
-            name:"Tát Nhật Lãng Rực Rỡ (火红的萨日朗)__Yếu Bất Yếu Mãi Thái__500" , 
+            name:"Tát Nhật Lãng Rực Rỡ (火红的萨日朗)__Yếu Bất Yếu Mãi Thái__500" ,
             url: "Musics/Tát Nhật Lãng Rực Rỡ (火红的萨日朗)__Yếu Bất Yếu Mãi Thái__500.m4a"},
-        { 
-            artist:"Ice Paper", 
-            name: "Tâm Lặng Như Nước (心如止水)", 
+        {
+            artist:"Ice Paper",
+            name: "Tâm Lặng Như Nước (心如止水)",
             url: "Musics/Tâm Lặng Như Nước (心如止水)__Ice Paper__320.mp3"},
-        { 
+        {
             artist: "unknown",
-            name:"Vì Yêu Cứ Đâm Đầu__Min;   Đen;   JustaTee__500" , 
+            name:"Vì Yêu Cứ Đâm Đầu__Min;   Đen;   JustaTee__500" ,
             url: "Musics/Vì Yêu Cứ Đâm Đầu__Min;   Đen;   JustaTee__500.m4a"},
-        { 
-            artist:"Avicii; Aloe Blacc", 
-            name: "Wake Me Up", 
+        {
+            artist:"Avicii; Aloe Blacc",
+            name: "Wake Me Up",
             url: "Musics/wakemeup.mp3"},
-        { 
-            artist:"Avicii; Sandro Cavazza", 
-            name: "Without You", 
+        {
+            artist:"Avicii; Sandro Cavazza",
+            name: "Without You",
             url: "Musics/withoutyou.mp3"}
     ]
     var rotaleImg = $("#circle-album");
     function showList(){
         var txt = "";
         for(var i=0;i<songs.length;i++){
-
             songName = songs[i].name;
             if (songName.length>15){
-                songName = songs[i].name.substr(0,10)+"....";
+                songName = songName.substr(0,12)+"....";
             }
             txt+=
-                "<li class='list-track'><i class='fas fa-play'></i><span class='li-album-name' size='20'>" + songName+ "</span><span class='leng-track'>"+"03:30"+"</span>";
+                "<li class='list-track' title='"+i+"' ><i class='fas fa-play'></i><span class='li-album-name' size='20'>" + songName+ "</span><span class='leng-track'>"+songs[i].artist.substr(0,15)+"</span>";
         }
         document.getElementById("song-list").innerHTML = txt;
     }
@@ -166,9 +165,9 @@ $(function(){
     function updateHoverTime(event){
         seekbarPos = seekArea.offset();
         seekTime = event.clientX-seekbarPos.left;
-        seekLoc = audio.duration * (seekTime /seekArea.outerWidth());
+        seekLoc = audio.duration * (seekTime /280);
         cM = seekLoc / 60;
-
+//        console.log(event.clientX);
 		ctMinutes = Math.floor(cM);
 		ctSeconds = Math.floor(seekLoc - ctMinutes * 60);
 		if( (ctMinutes < 0) || (ctSeconds < 0) )
@@ -185,7 +184,7 @@ $(function(){
             insTime.text('--:--');
         else
 		    insTime.text(ctMinutes+':'+ctSeconds);
-        insTime.css('left',seekTime*2);
+        insTime.css('left',seekTime);
     }
 // update current time
 var curMinutes,curSeconds,durMinutes,durSeconds,playProgress;
@@ -212,49 +211,108 @@ var tName = $("#player-track");
             durSeconds = '0'+durSeconds;
 
         seekBar.width(playProgress+'%');
+        if(playProgress == 100) 
+        {
+            updateList(1);
+            if( currentSong <songs.length)
+                currentSong+=1;
+            tName.text(songs[currentSong].name.substr(0,20));
+            audio.src = songs[currentSong].url;
+            rotaleImg.addClass('active');
+            i.attr('class','fas fa-pause');
+            updateList(0);
+            audio.play();
+            $(audio).on("timeupdate", updateCurTime);
+        }
         tProgress.text(curMinutes+':'+curSeconds);
         tTime.text(durMinutes+':'+durSeconds);
     }
     var x= $('li.list-track');
-
-    function updateList(){
-        x.eq(currentSong).addClass('active');
+    var tFlag;
+    function updateList(tFlag){
+        // alert("#q");
+        var x= $('li.list-track');
+        if (tFlag==0){
+            
+            x.eq(currentSong).addClass('active');
+            iaa = x.eq(currentSong).find('i');
+            iaa.attr('class','fas fa-pause');
+        }
+        else{
+            x.eq(currentSong).removeClass('active');
+            iaa = x.eq(currentSong).find('i');
+            iaa.attr('class','fas fa-play');
+        }
     }
+    function selectTrack(index){
+        updateList(1);
+        currentSong = index;
+        tName.text(songs[currentSong].name.substr(0,25));
+        audio.src = songs[currentSong].url;
+        rotaleImg.addClass('active');
+        i.attr('class','fas fa-pause');
+        updateList(0);
+        audio.play();
+        $(audio).on("timeupdate", updateCurTime);
+    }
+    
 // khoi tao audio
     function initAudio(){
         //??
         audio.src = songs[currentSong].url;
-        tName.text(songs[currentSong].name.substr(0,15));
+        updateList(1);
+//        selectTrack(3);
+        tName.text(songs[currentSong].name.substr(0,25));
         //done
         playPauseButton.on('click',playPauseClick);
         seekArea.mousemove(function(e){ updateHoverTime(e);});
         seekArea.click(playFromClickseek);
-        //
+        showList();
+        $('li').click(function() {
+           selectTrack(this.title);
+        });
+        
+        
+//
+
+        //done
         nextButton.on('click',function(){
-            currentSong+=1;
-            tName.text(songs[currentSong].name.substr(0,15));
+            updateList(1);
+            if( currentSong <songs.length)
+                currentSong+=1;
+            tName.text(songs[currentSong].name.substr(0,20));
             audio.src = songs[currentSong].url;
+            rotaleImg.addClass('active');
             i.attr('class','fas fa-pause');
+            updateList(0);
             audio.play();
-            updateList();
             $(audio).on("timeupdate", updateCurTime);
         });
-        prevButton.on('click',function(){
-            currentSong-=1;
-            audio.src = songs[currentSong].url;
-            tName.text(songs[currentSong].name.substr(0,15));
-            i.attr('class','fas fa-pause');
-            audio.play();
-            updateList();
-            $(audio).on("timeupdate", updateCurTime);
-        });;
+        
         //done
-        showList();
-        // done 1/3
+        prevButton.on('click',function(){
+            updateList(1);
+            if( currentSong >0)
+                currentSong-=1;
+            audio.src = songs[currentSong].url;
+            tName.text(songs[currentSong].name.substr(0,20));
+            rotaleImg.addClass('active');
+            i.attr('class','fas fa-pause');
+            updateList(0);
+            audio.play();
+            $(audio).on("timeupdate", updateCurTime);
+        });
+        
+        
 
-        // updateCurTime();
+
         $(audio).on("timeupdate", updateCurTime);
     }
     initAudio();
-
 })
+
+
+
+//$('li').click(function() {
+//   alert(this.id);
+//});
